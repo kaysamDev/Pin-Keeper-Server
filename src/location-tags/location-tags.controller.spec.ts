35927302edd556dd4394632503ba41807da761e0
kaysamDev <samuelmensqh@gmail.com>
2026-04-21 15:22:03 +0000

    build fix 001

--- src/location-tags/location-tags.controller.spec.ts
+++ src/location-tags/location-tags.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationTagsController } from './location-tags.controller';
 import { LocationTagsService } from './location-tags.service';
+import { AuthGuard } from '../models/auth/auth.guard';
 
 describe('LocationTagsController', () => {
   let controller: LocationTagsController;
+  const locationTagsServiceMock = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LocationTagsController],
-      providers: [LocationTagsService],
-    }).compile();
+      providers: [
+        {
+          provide: LocationTagsService,
+          useValue: locationTagsServiceMock,
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate: jest.fn().mockReturnValue(true) })
+      .compile();
 
     controller = module.get<LocationTagsController>(LocationTagsController);
   });
